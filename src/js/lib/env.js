ba5b1f14dcd3a084dff01ed8aa7a19f5b7a28cad
oh <oh@jig.jp>
2017-07-06 15:28:04 +0900

    env.js move to js/lib

---
+++ src/js/lib/env.js
@@ -0,0 +1,10 @@
+
+var env = {
+  sparql_endpoint: "https://sparql.opendata.cc/data/sparql",
+  googlemaps_api_key: "AIzaSyBHJFFJhajwYTL42wef39NSpiydZczlz5E",
+  app_name: "5star opendatamap"
+};
+
+var getEnv = function() {
+  return env;
+};
