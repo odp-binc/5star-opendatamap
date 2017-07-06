11549eb7585aff196a34edbf69a879910f4b9ff1
oh <oh@jig.jp>
2017-07-06 15:22:56 +0900

    replace api key

--- src/js/env.js
+++ src/js/env.js
@@ -1,7 +1,7 @@
 
 var env = {
   sparql_endpoint: "https://sparql.opendata.cc/data/sparql",
-  googlemaps_api_key: "AIzaSyBSiOhuA3rWEg35aMMkbRl0KwTP8tWueRs",
+  googlemaps_api_key: "AIzaSyBHJFFJhajwYTL42wef39NSpiydZczlz5E",
   app_name: "5star opendatamap"
 };
 
