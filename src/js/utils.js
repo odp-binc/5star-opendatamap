3cfd7d59ed40467f2482b8e7b03c2568e1295c90
oh <oh@jig.jp>
2017-08-23 14:59:48 +0900

    for IE

--- src/js/utils.js
+++ src/js/utils.js
@@ -20,3 +20,11 @@ var join = function(target, separator) {
   }
   return target;
 };
+
+if (!String.prototype.startsWith) {
+  String.prototype.startsWith = function(searchString, position){
+    position = position || 0;
+    return this.substr(position, searchString.length) === searchString;
+  };
+}
+
